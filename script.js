5d01f49820b21227d1eb5d29cefb34f2372e990d
KauanIzidoro <cnttbussineskauan@gmail.com>
2024-06-16 23:23:40 -0300

    :tada: init: create repository

---
+++ script.js
@@ -0,0 +1,9 @@
+//function for show card about semester
+function showInfo(id){
+    const infocard = document.getElementById(id);
+    infocard.style.display = infocard.style.display === 'none' || 
+    infocard.style.display === '' ? 'block' : 'none';
+}
+
+
+
